80f4cc0b629aa670ff17a3a3beb8904cf4fe0cd1
JarrodKane <kajarrod@gmail.com>
2023-02-02 16:06:59 +1100

    Changing features

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,16 +1,20 @@
 module.exports = {
 	stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx|svelte)'],
+	// stories: ['../src/**/*.stories.@(js|jsx|ts|tsx|svelte)'],
 	addons: [
 		'@storybook/addon-links',
 		'@storybook/addon-essentials',
 		'@storybook/addon-interactions',
 		'@storybook/addon-svelte-csf'
 	],
+	core: {
+		builder: '@storybook/builder-vite'
+	},
 	framework: '@storybook/sveltekit',
 	features: {
-		storyStoreV7: true
+		storyStoreV7: false
 	},
 	docs: {
-		autodocs: true
+		autodocs: false
 	}
 };
